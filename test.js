95a133e885970c06321d13e672ecf026a1a766ee
cindyle <cinditized@gmail.com>
2019-09-17 17:22:21 -0700

    second commit

---
+++ test.js
@@ -0,0 +1,12 @@
+//Do you sleep enough?
+
+let hoursOfSleep = 5;
+
+if (hoursOfSleep < 6) {
+    console.log("You need more sleep");
+} else if (hoursOfSleep < 8) {
+    console.log("You sleep enough");
+} else {
+    console.log("You sleep too much");
+    }
+
